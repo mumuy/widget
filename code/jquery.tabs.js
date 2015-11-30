62761f2ade72ca5b52d37fb0aef5c830e84ec9e2
郑浩乐 <89932980@qq.com>
2015-11-30 10:56:12 +0800

    updated 添加淡入淡出切换效果参数

--- code/jquery.tabs.js
+++ code/jquery.tabs.js
@@ -12,11 +12,12 @@
             getApi = getApi||function(){};
         }
         var defaults = {
-            contentCls: "content",      //内容列表的class
-            navCls: "nav",              //导航列表的class
-            activeCls: "active",        //导航选中时的class
-            triggerType: "mouse",       //切换时的触发事件
-            triggerCondition: "*",      //导航项的条件
+            contentCls: 'content',      //内容列表的class
+            navCls: 'nav',              //导航列表的class
+            activeCls: 'active',        //导航选中时的class
+            effect:'none',              //切换的效果
+            triggerType: 'mouse',       //切换时的触发事件
+            triggerCondition: '*',      //导航项的条件
             activeIndex: 0,             //默认选中导航项的索引
             beforeEvent: function() {   //切换前执行,返回flase时不移动;传入一个对象,包含：target当前导航项对象,tabs导航列表对象,panels内容列表对象,index当前导航项索引,event事件对象;
             },
@@ -27,18 +28,50 @@
         return this.each(function() {
             //对象定义
             var $this = $(this);
-            var $panels = $this.find("." + options.contentCls).children();
+            var $content = $this.find("." + options.contentCls);
+            var $panels = $content.children();
             var $triggers = $this.find("." + options.navCls + ">" + options.triggerCondition);
             //全局变量
             var _api = {};
+            var _size = $panels.length;
+            var _index = options.activeIndex;
             options.triggerType += options.triggerType === "mouse" ? "enter" : "";  //使用mouseenter防止事件冒泡
-            //函数
-            _api.select = function(i){
-                $triggers.removeClass(options.activeCls).eq(i).addClass(options.activeCls);
-                $panels.hide().eq(i).show();
+            //样式
+            if(options.effect=='fade'){
+                $content.css('position','relative');
+                $panels.css('position','absolute');
+            }
+            //选择某标签
+            _api.select = function(index){
+                $triggers.removeClass(options.activeCls).eq(index).addClass(options.activeCls); 
+                switch(options.effect){
+                    case 'fade':
+                        if(_index!=index){
+                            var $select = $panels.eq(_index).css('z-index',_size+1);
+                            $panels.each(function(i){
+                                if(i!=_index){
+                                    $(this).css('z-index',(index+_size-i-1)%_size+1);
+                                }
+                            });
+                            $select.fadeOut('normal',function(){
+                                $select.css({
+                                    'display':'block',
+                                    'z-index':(index+_size-_index-1)%_size+1
+                                });
+                                _index = index;
+                            });
+                        }else{
+                            $panels.each(function(i){
+                                $(this).css('z-index',(index+_size-i-1)%_size+1);
+                            });
+                        }
+                        break;
+                    default:
+                        $panels.hide().eq(index).show();        
+                }
             }
             //初始化  
-            _api.select(options.activeIndex);   //默认选中状态
+            _api.select(_index);   //默认选中状态
             $triggers.bind(options.triggerType, function(e) { //事件绑定
                 var i = $triggers.index($(this));
                 var status = {
