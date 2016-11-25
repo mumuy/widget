04bfb647f344da019d6843a4f3b307a76c2aeae5
mumuy <zhenghaole1990@126.com>
2016-11-25 11:11:21 +0800

    恢复

--- code/jquery.citys.js
+++ code/jquery.citys.js
@@ -22,8 +22,8 @@
             getApi = getApi||function(){};
         }
         var defaults = {
-            dataUrl:'http://passer-by.com/data_location/list.jsonp',     //数据库地址
-            dataType:'jsonp',          //数据库类型:'json'或'jsonp'
+            dataUrl:'http://passer-by.com/data_location/list.json',     //数据库地址
+            dataType:'json',          //数据库类型:'json'或'jsonp'
             provinceField:'province', //省份字段名
             cityField:'city',         //城市字段名
             areaField:'area',         //地区字段名
