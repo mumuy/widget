59cfe5345d55ac2ecad6243afe7d691ac84b2d00
PASSER-BY <zhenghaole1990@126.com>
2022-11-28 18:49:20 +0800

    updated 添加新插件

---
+++ code/jquery.tablesort.js
@@ -0,0 +1,134 @@
+/**
+ * jquery.tablesort.js 1.0
+ * http://jquerywidget.com
+ */
+ ;(function (factory) {
+    if (typeof define === "function" && (define.amd || define.cmd) && typeof jQuery == 'undefined'){
+        // AMD或CMD
+        define(['jquery'],function(){
+            factory(jQuery);
+            return jQuery;
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                } else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        //Browser globals
+        factory(jQuery);
+    }
+ }(function ($) {
+    $.fn.tablesort = function(parameter,getApi) {
+        if(typeof parameter == 'function'){ //重载
+            getApi = parameter;
+            parameter = {};
+        }else{
+            parameter = parameter || {};
+            getApi = getApi||function(){};
+        }
+        var defaults = {
+            /* 节点绑定 */
+            sortCls: 'sortabled',       // 排序的class
+            ascCls:'sort-asc',          // 升序
+            descCls:'sort-desc',        // 降序
+            activeCls:'active',
+            onSortEnd: function() {     // 排序后触发
+            }
+        };
+        var options = $.extend({}, defaults, parameter);
+        return this.each(function() {
+            var $table = $(this);
+            var thMap = [];
+            var thHash = {};
+            $table.find('thead tr').each(function(tr_index){
+                thMap[tr_index] = []; 
+            });
+            $table.find('thead tr').each(function(tr_index){
+                var $tr = $(this);
+                $tr.find('th,td').each(function(th_index){
+                    var $th = $(this);
+                    var rowspan = +$th.attr('rowspan')||1;
+                    var colspan = +$th.attr('colspan')||1;
+                    for(var m=0;m<thMap[tr_index].length;m++){
+                        if(typeof thMap[tr_index][m]=='undefined'){
+                            break;
+                        }
+                    }
+                    for(var i=0;i<rowspan;i++){
+                        for(var j=0;j<colspan;j++){
+                            thMap[tr_index+i][m+j] = tr_index+','+th_index;
+                            thHash[tr_index+','+th_index] = {
+                                'row':tr_index+i,
+                                'col':m+j
+                            };
+                        }
+                    }
+                });
+            });
+            var sortTableByIndex = function(index,type){
+                var type = type||'up';
+                var $bd_thead = $table.find('thead');
+                var $bd_tbody = $table.find('tbody');
+                $bd_tbody.find('td').removeClass(options.activeCls).filter(':nth-child('+(index+1)+')').addClass(options.activeCls);
+                var $items = $bd_tbody.find('tr');
+                var $temps = $items.clone();
+                var list = $.map($items,function(item,i){
+                    var $item = $(item);
+                    var value = $item.find('td').eq(index).data('value')||0;
+                    return {
+                        'index':i,
+                        'value':value
+                    };
+                });
+                if(type=='up'){
+                    list.sort(function(item1,item2){
+                        if(isNaN(item1['value'])){
+                            return item1['value'].localeCompare(item2['value']);
+                        }else{
+                            return item1['value']-item2['value'];
+                        }
+                    });
+                }else{
+                    list.sort(function(item1,item2){
+                        if(isNaN(item1['value'])){
+                            return item2['value'].localeCompare(item1['value']);
+                        }else{
+                            return item2['value']-item1['value'];
+                        }
+                    });
+                }
+                $items.each(function(index){
+                    var $this = $(this);
+                    var $temp = $temps.eq(list[index]['index']);
+                    $this.replaceWith($temp);
+                });
+            };
+            $table.on('click','th.'+options.sortCls,function(){
+                var $th = $(this);
+                var $tr = $th.parent();
+                var tr_index = $tr.index();
+                var th_index = $th.index();
+                var hash = thHash[tr_index+','+th_index];
+                if($th.hasClass(options.descCls)){
+                    $table.find('th').removeClass(options.descCls).removeClass(options.ascCls);
+                    $th.addClass(options.ascCls);
+                    sortTableByIndex(hash['col'],'up');
+                }else{
+                    $table.find('th').removeClass(options.descCls).removeClass(options.ascCls);
+                    $th.addClass(options.descCls);
+                    sortTableByIndex(hash['col'],'down');
+                }
+                options.onSortEnd(th_index);
+            });
+        });
+    };
+}));
