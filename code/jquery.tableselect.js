e982c4abc228e73a49eefd93c3b5dc8f79182a7e
zhenghaole <89932980@qq.com>
2023-08-25 15:52:38 +0800

    updated 单元格范围选择bug修复

--- code/jquery.tableselect.js
+++ code/jquery.tableselect.js
@@ -122,92 +122,6 @@
                     return false;
                 }
             };
-            // 获取从大范围排除小范围后的范围
-            var getRemainRange = function (child_range, parent_range) {
-                var left = Math.min(child_range['from'][0], child_range['to'][0]);
-                var top = Math.min(child_range['from'][1], child_range['to'][1]);
-                var right = Math.max(child_range['from'][0], child_range['to'][0]);
-                var bottom = Math.max(child_range['from'][1], child_range['to'][1]);
-                // 上方
-                if (parent_range['from'][0] == parent_range['to'][0] && parent_range['from'][1] > parent_range['to'][1]) {
-                    return {
-                        'from':parent_range['from'],
-                        'to':[left,buttom+1]
-                    };
-                // 下方
-                }else if (parent_range['from'][0] == parent_range['to'][0] && parent_range['from'][1] < parent_range['to'][1]) {
-                    return {
-                        'from':parent_range['from'],
-                        'to':[left,top-1]
-                    };
-                // 左方
-                }else if (parent_range['from'][0] > parent_range['to'][0] && parent_range['from'][1] == parent_range['to'][1]) {
-                    return {
-                        'from':parent_range['from'],
-                        'to':[right+1,top]
-                    };
-                // 右方
-                }else if (parent_range['from'][0] < parent_range['to'][0] && parent_range['from'][1] == parent_range['to'][1]) {
-                    return {
-                        'from':parent_range['from'],
-                        'to':[left-1,top]
-                    };
-                // 右下
-                }else if (parent_range['from'][0] < parent_range['to'][0] && parent_range['from'][1] < parent_range['to'][1]) {
-                    var x = Math.max(left-1,parent_range['from'][0]);
-                    var y = Math.max(top-1,parent_range['from'][1]);
-                    if(right==parent_range['to'][0]){
-                        y = parent_range['to'][1];
-                    }else if(bottom==parent_range['to'][1]){
-                        x = parent_range['to'][0];
-                    }
-                    return {
-                        'from':parent_range['from'],
-                        'to':[x,y]
-                    };
-                // 左下
-                } else if (parent_range['from'][0] > parent_range['to'][0] && parent_range['from'][1] < parent_range['to'][1]) {
-                    var x = Math.min(right+1,parent_range['from'][0]);
-                    var y = Math.max(top-1,parent_range['from'][1]);
-                    if(left==parent_range['to'][0]){
-                        y = parent_range['to'][1];
-                    }else if(bottom==parent_range['to'][1]){
-                        x = parent_range['to'][0];
-                    }
-                    return {
-                        'from':parent_range['from'],
-                        'to':[x,y]
-                    };
-                // 右上
-                } else if (parent_range['from'][0] < parent_range['to'][0] && parent_range['from'][1] > parent_range['to'][1]) {
-                    var x = Math.max(left-1,parent_range['from'][0]);
-                    var y = Math.min(bottom+1,parent_range['from'][1]);
-                    if(right==parent_range['to'][0]){
-                        y = parent_range['to'][1];
-                    }else if(top==parent_range['to'][1]){
-                        x = parent_range['to'][0];
-                    }
-                    return {
-                        'from':parent_range['from'],
-                        'to':[x,y]
-                    };
-                // 左上
-                } else if (parent_range['from'][0] > parent_range['to'][0] && parent_range['from'][1] > parent_range['to'][1]) {
-                    var x = Math.min(right+1,parent_range['from'][0]);
-                    var y = Math.min(bottom+1,parent_range['from'][1]);
-                    if(left==parent_range['to'][0]){
-                        y = parent_range['to'][1];
-                    }else if(top==parent_range['to'][1]){
-                        x = parent_range['to'][0];
-                    }
-                    return {
-                        'from':parent_range['from'],
-                        'to':[x,y]
-                    };
-                } else {
-                    return null;
-                }
-            };
             var selectRange = function (param, callback) {
                 var callback = callback || function () { };
                 var cellList = [];
@@ -223,61 +137,138 @@
                 var $abledTds = $tds.filter(function () {
                     return !$(this).hasClass(options.disabledCls);
                 });
-                var $disabledTds = $tds.filter(function () {
-                    return $(this).hasClass(options.disabledCls);
-                });
                 // 动态判断限制范围
                 var limit_range = options.onSelecting(selected_range);
                 if(limit_range){
                     selected_range = limit_range;
                 }
-                // 遇到不可选单元格的处理
-                $disabledTds.each(function(){
+                // 已选单元格
+                var selected_ranges = $tds.filter(function () {
+                    return !$(this).hasClass(options.disabledCls);
+                }).map(function(){
                     var $temp = $(this);
                     var t_fromKey = $temp.data('from');
                     var t_toKey = $temp.data('to');
                     var t_from = t_fromKey.split(':').map(function (value) { return +value; });
                     var t_to = t_toKey.split(':').map(function (value) { return +value; });
-                    let child_range = { from: t_from, to: t_to };
-                    if(isInRange(child_range,selected_range)){
-                        selected_range = getRemainRange(child_range,selected_range);
+                    return { from: t_from, to: t_to };
+                }).get().filter(function(range){
+                    return isInRange(range,selected_range);
+                }).sort(function(range1,range2){
+                    var value1 = 0;
+                    var value2 = 0;
+                    // 左上
+                    if(selected_range['from'][0]<=selected_range['to'][0]&&selected_range['from'][1]<=selected_range['to'][1]){
+                        // console.log('[左上]');
+                        value1 = range1['from'][0]*10000+range1['from'][1];
+                        value2 = range2['from'][0]*10000+range2['from'][1];
+                        return value1-value2?1:-1;
+                    // 右上
+                    }else if(selected_range['from'][0]>=selected_range['to'][0]&&selected_range['from'][1]<=selected_range['to'][1]){
+                        // console.log('[右上]');
+                        value1 = range1['to'][0]*10000+range1['from'][1];
+                        value2 = range2['to'][0]*10000+range2['from'][1];
+                        return value1-value2?1:-1;
+                    // 左下
+                    }else if(selected_range['from'][0]<=selected_range['to'][0]&&selected_range['from'][1]>=selected_range['to'][1]){
+                        // console.log('[左下]');
+                        value1 = range1['from'][0]*10000+range1['to'][1];
+                        value2 = range2['from'][0]*10000+range2['to'][1];
+                        return value1-value2?-1:1;
+                    // 右下
+                    }else if(selected_range['from'][0]>=selected_range['to'][0]&&selected_range['from'][1]>=selected_range['to'][1]){
+                        // console.log('[右下]');
+                        value1 = range1['to'][0]*10000+range1['to'][1];
+                        value2 = range2['to'][0]*10000+range2['to'][1];
+                        return value1-value2?-1:1;
                     }
+                    return value1-value2?-1:1;
                 });
+                // 不可选单元格的处理
+                var disabled_ranges = $tds.filter(function () {
+                    return $(this).hasClass(options.disabledCls);
+                }).map(function(){
+                    var $temp = $(this);
+                    var t_fromKey = $temp.data('from');
+                    var t_toKey = $temp.data('to');
+                    var t_from = t_fromKey.split(':').map(function (value) { return +value; });
+                    var t_to = t_toKey.split(':').map(function (value) { return +value; });
+                    return { from: t_from, to: t_to };
+                }).get();
+
                 // 遇到夸行或跨列单元格的处理
                 var getOuterRange = function (range) {
-                    $abledTds.each(function () {
-                        var $temp = $(this);
-                        var t_fromKey = $temp.data('from');
-                        var t_toKey = $temp.data('to');
-                        var t_from = t_fromKey.split(':').map(function (value) { return +value; });
-                        var t_to = t_toKey.split(':').map(function (value) { return +value; });
-                        if (isInRange({ from: t_from, to: t_to }, range)) {
-                            cellList.push({
-                                'from': t_from,
-                                'to': t_to,
-                            });
+                    var getRange = function(){
+                        $abledTds.each(function () {
+                            var $temp = $(this);
+                            var t_fromKey = $temp.data('from');
+                            var t_toKey = $temp.data('to');
+                            var t_from = t_fromKey.split(':').map(function (value) { return +value; });
+                            var t_to = t_toKey.split(':').map(function (value) { return +value; });
+                            if (isInRange({ from: t_from, to: t_to }, range)) {
+                                cellList.push({
+                                    'from': t_from,
+                                    'to': t_to,
+                                });
+                            }
+                        });
+                        var outer_from = [Math.min.apply(null, cellList.map(function (item) {
+                            return Math.min(item['from'][0],item['to'][0]);
+                        })), Math.min.apply(null, cellList.map(function (item) {
+                            return Math.min(item['from'][1],item['to'][1]);
+                        }))];
+                        var outer_to = [Math.max.apply(null, cellList.map(function (item) {
+                            return Math.max(item['from'][0],item['to'][0]);
+                        })), Math.max.apply(null, cellList.map(function (item) {
+                            return Math.max(item['from'][1],item['to'][1]);
+                        }))];
+                        return {
+                            from: outer_from,
+                            to: outer_to
+                        };
+                    };
+                    var temp_range = JSON.parse(JSON.stringify(selected_range));
+                    var outer_range = getRange(temp_range);
+                    while (outer_range['from'][0] != temp_range['from'][0] || outer_range['from'][1] != temp_range['from'][1] || outer_range['to'][0] != temp_range['to'][0] || outer_range['to'][1] != temp_range['to'][1]) {
+                        temp_range = outer_range;
+                        outer_range = getRange(outer_range);
+                    }
+                    return outer_range;
+                };
+                // 范围内单元格逐一检查
+                var temp_range = null;
+                var outer_range = null;
+                var isDisabled = false;
+                selected_ranges.forEach(function(child_range){
+                    var range = {
+                        from:[
+                            Math.min(child_range['from'][0],child_range['to'][0]),
+                            Math.min(child_range['from'][1],child_range['to'][1]),
+                        ],
+                        to:[
+                            Math.max(child_range['from'][0],child_range['to'][0]),
+                            Math.max(child_range['from'][1],child_range['to'][1]),
+                        ]
+                    };
+                    if(!temp_range){
+                        temp_range = range;
+                    }else{
+                        temp_range['from'][0] = Math.min(temp_range['from'][0],range['from'][0]);
+                        temp_range['from'][1] = Math.min(temp_range['from'][1],range['from'][1]);
+                        temp_range['to'][0] = Math.max(temp_range['to'][0],range['to'][0]);
+                        temp_range['to'][1] = Math.max(temp_range['to'][1],range['to'][1]);
+                    }
+                    var out_range = getOuterRange(temp_range);
+                    disabled_ranges.forEach(function(range){
+                        if(isInRange(range,out_range)){
+                            isDisabled = true;
                         }
                     });
-                    var outer_from = [Math.min.apply(null, cellList.map(function (item) {
-                        return item['from'][0];
-                    })), Math.min.apply(null, cellList.map(function (item) {
-                        return item['from'][1];
-                    }))];
-                    var outer_to = [Math.max.apply(null, cellList.map(function (item) {
-                        return item['to'][0];
-                    })), Math.max.apply(null, cellList.map(function (item) {
-                        return item['to'][1];
-                    }))];
-                    return {
-                        from: outer_from,
-                        to: outer_to
-                    };
-                };
-                var outer_range = getOuterRange(selected_range);
-                while (outer_range['from'][0] != selected_range['from'][0] || outer_range['from'][1] != selected_range['from'][1] || outer_range['to'][0] != selected_range['to'][0] || outer_range['to'][1] != selected_range['to'][1]) {
-                    selected_range = outer_range;
-                    outer_range = getOuterRange(selected_range);
-                }
+                    if(!isDisabled){
+                        outer_range = out_range;
+                    }
+                });
+                // 样式设置
                 $abledTds.each(function () {
                     var $temp = $(this);
                     var t_fromKey = $temp.data('from');
@@ -457,3 +448,4 @@
         });
     };
 }));
+
