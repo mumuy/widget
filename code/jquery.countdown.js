62127992cb527d45e01eda9f01fb8a232c3ffe10
mumuy <zhenghaole1990@126.com>
2026-04-09 13:56:19 +0800

    updated 倒计时输出跳转，不再用当前时间，而是剩余时间间隔

--- code/jquery.countdown.js
+++ code/jquery.countdown.js
@@ -1,5 +1,5 @@
 /**
- * jquery.countdown.js 1.0
+ * jquery.countdown.js 1.1
  * http://jquerywidget.com
  */
 ; (function (factory) {
@@ -52,105 +52,111 @@
         }
         return this.each(function (i) {
             let $this = $(this);
+            const text = $this.text().trim();
+            const starttime = text.match(/((\d{4})[\/\-年](\d{2})[\/\-月](\d{2})[日]?\s+)?\d{2}:\d{2}(:\d{2})?|\d+/)?.[0]||'';
             let defaults = {
-                'format': 'hh:mm:ss',					//格式
-                'starttime': $this.text(),				//开始时间
-                'endtime': '',							//结束时间
-                'interval': 1000,						//多久倒计时一次 单位：ms
+                'format': 'HH:mm:ss',					// 格式
+                'starttime': starttime,	                // 开始时间
+                'endtime': '',							// 结束时间
+                'interval': 1000,						// 多久倒计时一次 单位：ms
                 'disabledCls': 'disabled',
-                'auto': true,							//是否默认自动计数
-                'countEach': function (time) {			//每单位时间出发事件,传入一个对象，包含时间信息(month)和时间格式化输出(format)
-                    $this.text(time['format']);
+                'auto': true,							// 是否默认自动计数
+                'countEach': function (time) {			// 每单位时间出发事件,传入一个对象，包含时间信息(month)和时间格式化输出(format)
+                    $this.text(time['current']);
                 },
-                'countEnd': function (time) { }			//倒计时结束回调事件
+                'countEnd': function (time) { }			// 倒计时结束回调事件
             };
             let options = $.extend({}, defaults, parameter);
-            let _api = {};              //对外的函数接口
+            let _api = {};                              // 对外的函数接口
             let _hander = null;
             let _start = 0, _end = 0;
-            let isTime = isNaN(options.starttime) || isNaN(options.endtime); //是否时间表达式
+            let isTime = isNaN(options.starttime)||isNaN(options.endtime); // 是否时间表达式
+            let isReverse = false;
             let getTime = function () {
-                let date, format;
                 let time = _start;
                 let diff = _end - _start;
-                let offset_GMT = new Date().getTimezoneOffset();
-                let diff_format;
+                let format;
                 if (isTime) {
-                    date = new Date(time);
-                    format = timeFormat(options.format, time);
-                    diff_format = timeFormat(options.format, diff + offset_GMT * 60 * 1000);
-                } else {
-                    date = new Date();
-                    format = time / 1e3;
-                    diff_format = diff / 1e3;
-                }
+					format = timeFormat(options.format, time);
+				} else {
+					format = time / options.interval;
+				}
                 return {
-                    'year': date.getFullYear(),
-                    'month': date.getMonth() + 1,
-                    'day': date.getDate(),
-                    'hour': date.getHours(),
-                    'minute': date.getMinutes(),
-                    'second': date.getSeconds(),
-                    'quarter': Math.floor((date.getMonth() + 3) / 3),
-                    'microsecond': date.getMilliseconds(),
-                    'format': format,
-                    'distance': diff_format,
-                    'timestamp': diff
+                    'day': Math.floor(diff / 86400000),
+                    'hour': Math.floor(diff % 86400000 / 3600000),
+                    'minute': Math.floor(diff % 3600000 / 60000),
+                    'second': Math.floor(diff % 60000 / 1000),
+                    'microsecond': diff % 1000,
+                    'distance': diff,
+                    'current': format
                 };
             };
             let count = function () {
                 if (_hander) {
                     clearInterval(_hander);
                 }
-                options.countEach(getTime());
+                setTimeout(function () {
+                    options.countEach(getTime());
+                }, 0);
                 $this.addClass(options.disabledCls);
-                let isReverse = _start > _end ? true : false;
                 _hander = setInterval(function () {
                     if (isReverse) {
-                        _start -= options.interval;
-                        options.countEach(getTime());
+                        if (options.starttime) {
+                            _start -= options.interval;
+                        } else {
+                            _start = Date.now();
+                        }
+                        options.countEach(getTime(_start));
                         if (_start <= _end) {
                             clearInterval(_hander);
-                            $this.removeClass(options.disabledCls);
-                            options.countEnd(getTime());
+                            $this.removeClass(options.disableBtnCls);
+                            options.countEnd(getTime(_end));
                         }
                     } else {
                         _start += options.interval;
-                        options.countEach(getTime());
+                        if (options.starttime) {
+                            _start += options.interval;
+                        } else {
+                            _start = Date.now();
+                        }
+                        options.countEach(getTime(_start));
                         if (_start >= _end) {
                             clearInterval(_hander);
-                            $this.removeClass(options.disabledCls);
-                            options.countEnd(getTime());
+                            $this.removeClass(options.disableBtnCls);
+                            options.countEnd(getTime(_end));
                         }
                     }
                 }, options.interval);
             };
             _api.reset = function () {
-                if (isTime) {
-                    _start = options.starttime ? getTimestamp(options.starttime) : (+new Date());
-                    _end = getTimestamp(options.endtime);
-                } else {
-                    _start = options.starttime * 1e3;
-                    _end = options.endtime * 1e3;
+                if(isTime){
+                    _start = options.starttime ? (isNaN(options.starttime) ? getTimestamp(options.starttime) : options.starttime) : Date.now();
+                    _end = isNaN(options.endtime) ? getTimestamp(options.endtime) : options.endtime;
+                }else{
+                    _start = options.starttime*options.interval;
+					_end = options.endtime*options.interval;
                 }
+                isReverse = _start > _end ? true : false;
                 count();
             };
             _api.setStarttime = function (start) {
+                options.starttime = start;
                 isTime = isNaN(start);
-                if (isTime) {
-                    _start = getTimestamp(start);
-                } else {
-                    _start = start * 1e3;
-                }
+				if(isTime){
+					_start = options.starttime ? (isNaN(options.starttime) ? getTimestamp(options.starttime) : options.starttime) : Date.now();
+				}else{
+					_start = start*options.interval;
+				}
                 count();
             };
             _api.setEndtime = function (end) {
+                options.endtime = end;
                 isTime = isNaN(end);
-                if (isTime) {
-                    _end = getTimestamp(end);
-                } else {
-                    _end = end * 1e3;
-                }
+				if(isTime){
+                    _end = isNaN(options.endtime) ? getTimestamp(options.endtime) : options.endtime;
+                }else{
+					_end = end*options.interval;
+				}
                 count();
             };
             //初始化
@@ -161,20 +167,21 @@
         });
         function getTimestamp(str) {
             str = str.replace(/\-/g, '/');
-            return +new Date(str) || +new Date('1970/1/1 ' + str);
+            const today = (new Date()).toISOString().substring(0, 10);
+            return +new Date(str) || +new Date(`${today} ${str}`);
         }
         function timeFormat(fmt, timestamp) {
             let date = new Date(timestamp);
             let o = {
-                "M+": date.getMonth() + 1,                 //月份
-                "d+": date.getDate(),                    //日
-                "h+": date.getHours(),                   //小时
+                "M+": date.getMonth() + 1,               //月份
+                "D+": date.getDate(),                    //日
+                "H+": date.getHours(),                   //小时
                 "m+": date.getMinutes(),                 //分
                 "s+": date.getSeconds(),                 //秒
                 "q+": Math.floor((date.getMonth() + 3) / 3), //季度
                 "S": date.getMilliseconds()             //毫秒
             };
-            if (/(y+)/.test(fmt)) {
+            if (/(Y+)/.test(fmt)) {
                 fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
             }
             for (let k in o) {
